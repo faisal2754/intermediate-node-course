fa85e94d527a3adf2424733c29aeff278d8b41fb
Faisal <saleemfs27@gmail.com>
2021-09-18 11:51:18 +0200

    Basic setup

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
 
-app.use(bodyParser.json());
+const port = 8000;
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
-})
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`server is listening on port:${port}`);
+});
 
 // CREATE
-app.post('/users',(req,res)=>{
-  // User.create()
-})
+app.post('/users', (req, res) => {
+    // User.create()
+});
 
 app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  // User.findById()
-})
-// UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
-})
-// DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-})
+    // READ
+    .get((req, res) => {
+        // User.findById()
+    })
+    // UPDATE
+    .put((req, res) => {
+        // User.findByIdAndUpdate()
+    })
+    // DELETE
+    .delete((req, res) => {
+        // User.findByIdAndDelete()
+    });
